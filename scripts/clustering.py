1276de2fe9ed41b316f6b073f08465a5a680f2ea
PND <fujjisaaan@gmail.com>
2019-08-12 11:54:52 +0900

    Change the grouping and run the pipeline.

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -1,28 +1,22 @@
 import pickle
-import random
 from typing import List, Callable, Any
 
 import numpy
-from pandas import DataFrame, concat, read_pickle
+from pandas import DataFrame, read_pickle
 from sklearn.cluster import KMeans
-from toolz import partition_all
 
-from clustering_chat.utils import flatten, partition_df
 from clustering_chat.clustering import kmeans
+from clustering_chat.utils import flatten, partition_df
 
 
-def test_groups(df: DataFrame, test_group_size: int, test_group_cluster_num: int) -> List[DataFrame]:
+def test_groups(df: DataFrame, test_group_size: int) -> List[DataFrame]:
     videos = df.groupby('video.url')
     windows: List[DataFrame] = flatten([
         partition_df(df_video.sort_values(by='published_at'), test_group_size)
         for _, df_video in videos
     ])
 
-    random.seed(0)
-    random.shuffle(windows)
-
-    partitioned = partition_all(test_group_cluster_num, windows)
-    return list(map(concat, partitioned))
+    return windows
 
 
 def clustering_function(clustering_method: str, **clustering_options) -> Callable[[Any], List[int]]:
@@ -32,9 +26,9 @@ def clustering_function(clustering_method: str, **clustering_options) -> Callabl
         raise NotImplementedError(clustering_method)
 
 
-def main(dataset_path: str, output_path: str, test_group_size: int, test_group_cluster_num: int, **clustering_options):
+def main(dataset_path: str, output_path: str, test_group_size: int, **clustering_options):
     df_all = read_pickle(dataset_path)
-    dfs = test_groups(df_all, test_group_size, test_group_cluster_num)
+    dfs = test_groups(df_all, test_group_size)
 
     for df in dfs:
         document_vectors = numpy.stack(df['document_vector'].to_numpy())
@@ -51,9 +45,8 @@ if __name__ == '__main__':
     main(
         dataset_path='resources/document_vectors.pkl',
         output_path='resources/clustering_results.pkl',
-        test_group_size=10,
-        test_group_cluster_num=5,
-        n_clusters=5,
+        test_group_size=100,
+        n_clusters=10,
         random_state=0,
         n_jobs=4
     )
