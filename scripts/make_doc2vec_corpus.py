fc098d8a26ef63c0421f941d31305bf0de3afa55
PND <fujjisaaan@gmail.com>
2019-08-12 20:40:10 +0900

    Fix line breaks in Doc2Vec corpus.

--- scripts/make_doc2vec_corpus.py
+++ scripts/make_doc2vec_corpus.py
@@ -6,11 +6,11 @@ from clustering_chat.tokenizer import load_sentencepiece
 def main(tokenizer_path: str, train_data_path: str, out_path: str):
     tokenizer = load_sentencepiece(tokenizer_path)
 
-    with open(out_path, mode='w') as fp_out:
+    with open(out_path, mode='w', encoding='utf-8') as fp_out:
         with open(train_data_path, encoding='utf-8') as fp_in:
             for line in fp_in:
                 tokens = tokenizer.EncodeAsPieces(line)[1:]
-                out_line = ' '.join(tokens)
+                out_line = ' '.join(tokens) + '\n'
                 fp_out.write(out_line)
 
 
