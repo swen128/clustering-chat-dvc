de4d4dbf8cb8e9930ff57e292fc888d16821f764
PND <fujjisaaan@gmail.com>
2019-08-12 20:06:32 +0900

    Tokenize sentences in the dataset before training a Doc2Vec model.

---
+++ scripts/make_doc2vec_corpus.py
@@ -0,0 +1,22 @@
+from smart_open import open
+
+from clustering_chat.tokenizer import load_sentencepiece
+
+
+def main(tokenizer_path: str, train_data_path: str, out_path: str):
+    tokenizer = load_sentencepiece(tokenizer_path)
+
+    with open(out_path, mode='w') as fp_out:
+        with open(train_data_path, encoding='utf-8') as fp_in:
+            for line in fp_in:
+                tokens = tokenizer.EncodeAsPieces(line)[1:]
+                out_line = ' '.join(tokens)
+                fp_out.write(out_line)
+
+
+if __name__ == '__main__':
+    main(
+        tokenizer_path='resources/tokenizer/sentence_piece.model',
+        train_data_path='resources/train.csv',
+        out_path='resources/doc2vec_corpus.txt'
+    )
--- scripts/train_doc2vec.py
+++ scripts/train_doc2vec.py
@@ -3,9 +3,6 @@ from typing import List
 
 from gensim.models import Doc2Vec
 
-from clustering_chat.doc2vec import Corpus
-from clustering_chat.tokenizer import load_sentencepiece
-
 formatter = logging.Formatter(logging.BASIC_FORMAT)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
@@ -16,23 +13,15 @@ logger_gensim.setLevel(logging.INFO)
 logger_gensim.addHandler(stream_handler)
 
 
-def main(tokenizer_path: str, train_data_path: str, out_path: str, **doc2vec_train_options):
-    tokenizer = load_sentencepiece(tokenizer_path)
-
-    def tokenize(sentence: str) -> List[str]:
-        return tokenizer.EncodeAsPieces(sentence)[1:]
-
-    corpus = Corpus(train_data_path, preprocess=tokenize)
-    doc2vec = Doc2Vec(corpus, **doc2vec_train_options)
-
+def main(train_data_path: str, out_path: str, **doc2vec_train_options):
+    doc2vec = Doc2Vec(corpus_file=train_data_path, **doc2vec_train_options)
     doc2vec.save(out_path)
 
 
 if __name__ == '__main__':
     main(
-        tokenizer_path='resources/tokenizer/sentence_piece.model',
-        train_data_path='resources/train.csv',
+        train_data_path='resources/doc2vec_corpus.txt',
         out_path='resources/doc2vec_model',
-        workers=20,
+        workers=2,
         vector_size=100
     )
