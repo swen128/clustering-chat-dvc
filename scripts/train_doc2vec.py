d6fb2712b8194a7458e2957d4da7cbb233dcf470
PND <fujjisaaan@gmail.com>
2019-08-13 12:23:46 +0900

    Force Doc2Vec to save all the attributes of a model into separate files.

--- scripts/train_doc2vec.py
+++ scripts/train_doc2vec.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from pathlib import Path
 
 from gensim.models import Doc2Vec
 
@@ -15,13 +15,19 @@ logger_gensim.addHandler(stream_handler)
 
 def main(train_data_path: str, out_path: str, **doc2vec_train_options):
     doc2vec = Doc2Vec(corpus_file=train_data_path, **doc2vec_train_options)
-    doc2vec.save(out_path)
+
+    path = Path(out_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Setting `sep_limit=0` enforces it to save all the attributes of the Doc2Vec model into separate files.
+    # This ensures the static list of output files.
+    doc2vec.save(out_path, sep_limit=0)
 
 
 if __name__ == '__main__':
     main(
         train_data_path='resources/doc2vec_corpus.txt',
-        out_path='resources/doc2vec_model',
+        out_path='resources/doc2vec/model',
         workers=2,
         epochs=20,
         vector_size=100
