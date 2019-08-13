b3700bad200ba1360bfb3fedb1415d06cf6eff6b
PND <fujjisaaan@gmail.com>
2019-08-13 12:22:43 +0900

    Lower the number of workers for training Doc2Vec.

--- scripts/train_doc2vec.py
+++ scripts/train_doc2vec.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     main(
         train_data_path='resources/doc2vec_corpus.txt',
         out_path='resources/doc2vec_model',
-        workers=10,
+        workers=2,
         epochs=20,
         vector_size=100
     )
