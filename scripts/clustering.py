7bfb6cb7679eeb452e565e03e4881c4a98f5d567
PND <fujjisaaan@gmail.com>
2020-01-02 15:07:05 +0900

    Try agglomerative clustering.

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -3,9 +3,8 @@ from typing import List, Callable, Any
 
 import numpy
 from pandas import read_pickle
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
 
-from clustering_chat.clustering import kmeans
 from clustering_chat.utils import partition_df
 
 
@@ -16,7 +15,7 @@ def clustering_function(clustering_method: str, **clustering_options) -> Callabl
         raise NotImplementedError(clustering_method)
 
 
-def main(dataset_path: str, output_path: str, test_group_size: int, **clustering_options):
+def main(dataset_path: str, output_path: str, test_group_size: int, clustering_function):
     df_all = read_pickle(dataset_path)
     df_all.sort_values(by=['video.url', 'published_at'], inplace=True)
 
@@ -24,21 +23,19 @@ def main(dataset_path: str, output_path: str, test_group_size: int, **clustering
 
     for df in dfs:
         document_vectors = numpy.stack(df['document_vector'].to_numpy())
-        clustering_labels, closest_points_to_centroids = kmeans(document_vectors, **clustering_options)
-        df_indices = numpy.arange(len(df.index))
-        df['clustering_label'] = clustering_labels
-        df['is_representative'] = numpy.isin(df_indices, closest_points_to_centroids)
+        df['clustering_label'] = clustering_function(document_vectors)
 
     with open(output_path, mode='wb') as f:
         pickle.dump(dfs, f)
 
 
 if __name__ == '__main__':
+    # clustering_function = KMeans(n_clusters=20, random_state=0, n_jobs=4).fit_predict
+    clustering_function = AgglomerativeClustering(n_clusters=20, linkage='ward').fit_predict
+
     main(
         dataset_path='resources/document_vectors.pkl',
         output_path='resources/clustering_results.pkl',
         test_group_size=200,
-        n_clusters=20,
-        random_state=0,
-        n_jobs=4
+        clustering_function=clustering_function
     )
